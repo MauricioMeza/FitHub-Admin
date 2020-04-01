85e14d89ac4096a2b432f0aeaa0b42cd79558a23
Mauricio Meza <mmezab@unal.edu.co>
2020-04-01 17:14:57 -0500

    Eliminado Angular

--- fitHub-Admin/src/main/java/com/api/controladores/UsuarioControlador.java
+++ fitHub-Admin/src/main/java/com/api/controladores/UsuarioControlador.java
@@ -1,46 +1,44 @@
-package com.javatechie.spring.mongo.api.controlador;
+package com.api.controladores;
 
 import java.util.List;
 import java.util.Optional;
 
+import com.api.servicios.UsuarioServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.javatechie.spring.mongo.api.modelo.Usuario;
-import com.javatechie.spring.mongo.api.repositorio.UsuarioRepositorio;
+import com.api.modelos.Usuario;
 
 @RestController
-
 public class UsuarioControlador {
 	
 	@Autowired
-	private UsuarioRepositorio repositorio;
+	private UsuarioServicio servicio;
 	
 	@PostMapping("/agregarUsuario")
 	public String GuardarUsuario(@RequestBody Usuario usuario) {
-		repositorio.save(usuario);
+		servicio.saveUsuario(usuario);
 		return "Usuario añadido con id: "+ usuario.getId();
 	}
 	
 	@GetMapping("/encontrarTodosLosUsuarios")
 	public List<Usuario> getUsuarios(){
-		return repositorio.findAll();
+		return servicio.findAllUsuarios();
 	}
 	
 	@GetMapping("/encontrarUsuario/{Id}")
 	public Optional<Usuario> getUsuario(@PathVariable int id){
-		return repositorio.findById(id);
+		return servicio.finUsuarioById(id);
 	}
 	
 	@DeleteMapping("/borrarUsuario/{Id}")
 	public String borrarUsuario(@PathVariable int id){
-		repositorio.deleteById(id);
+		servicio.deleteUsuarioById(id);
 		return "El usuario con el Id: "+ id+" ha sido borrado";
 	}
 	
