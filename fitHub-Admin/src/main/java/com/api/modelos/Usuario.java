5204caf2167cec862a636f4db9a2ed98fffaf17a
Mauricio Meza <mmezab@unal.edu.co>
2020-04-03 17:18:41 -0500

    Modelo Usuario Modificado

--- fitHub-Admin/src/main/java/com/api/modelos/Usuario.java
+++ fitHub-Admin/src/main/java/com/api/modelos/Usuario.java
@@ -1,45 +1,37 @@
 package com.api.modelos;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-@Getter
-@Setter
-@ToString
 
 @Document(collection= "Usuario")
 public class Usuario {
 	
 	@Id
-	public int cedula;
-	public String nombre;
-	public String correo;
-	
-	
-	
-	public int getId() {
-		return cedula;
-		
-		
-	}
-	public void setId(int id) {
-		this.cedula = id;
-	}
-	public String getUsuario_Nombre() {
-		return nombre;
-	}
-	public void setUsuario_Nombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public String getUsuario_Apellido() {
-		return correo;
-	}
-	public void setCorreo(String correo) {
-		this.correo = correo;
-	}
-	
+	private int cedula;
+	private String nombre;
+	private String correo;
+	private String contrasena;
+	@DBRef
+	private Plan planActual;
+
+	public int getCedula() { return cedula;}
+	public void setCedula(int cedula) { this.cedula = cedula; }
+
+	public String getNombre() { return nombre; }
+	public void setNombre(String nombre) { this.nombre = nombre; }
+
+	public String getCorreo() { return correo; }
+	public void setCorreo(String correo) { this.correo = correo; }
+
+	public String getContrasena() { return contrasena; }
+	public void setContrasena(String contrasena) { this.contrasena = contrasena; }
+
+	public Plan getPlanActual() { return planActual; }
+	public void setPlanActual(Plan planActual) { this.planActual = planActual; }
 }
