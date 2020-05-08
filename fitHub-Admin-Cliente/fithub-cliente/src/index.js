46153eb67fc6a01b2b15e23ce2f419cfd371b312
Mauricio Meza <mmezab@unal.edu.co>
2020-05-08 03:16:40 -0500

    Redux-JWT Implementado

--- fitHub-Admin-Cliente/fithub-cliente/src/index.js
+++ fitHub-Admin-Cliente/fithub-cliente/src/index.js
@@ -4,8 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import {Provider} from "react-redux"
+import store from "./reducers/store"
+
+
+
+
 ReactDOM.render(
-    <App />,
+  <Provider store = {store}>
+    <App />
+  </Provider>,  
   document.getElementById('root')
 );
 
--- fitHub-Admin-Cliente/fithub-cliente/src/services/ClaseService.js
+++ fitHub-Admin-Cliente/fithub-cliente/src/services/ClaseService.js
@@ -1,7 +1,11 @@
 import Axios from "axios";
 import AuthService from "./AuthService";
 
-const token = AuthService.getCurrentUser().Authorization
+var token = ""
+if(AuthService.getCurrentUser() != null){
+  token = AuthService.getCurrentUser().Authorization
+}
+
 const API_URL = "http://localhost:8080/Admin/";
 
 class ClaseService {
@@ -13,21 +17,28 @@ class ClaseService {
         instructor: instructor,
       })
 
-      return Axios.post( API_URL + "agregarSesion", sesion, {headers:{"Content-Type" : "application/json"}})
+      return Axios.post( API_URL + "agregarSesion", sesion, 
+      {headers:{"Content-Type" : "application/json",
+                "Authorization": token
+              }})
   }
 
   getClases(){
     
-    return Axios.get(API_URL + "buscarTodasSesiones" /*, {headers:{"Authorization": token}}*/)
+    return Axios.get(API_URL + "buscarTodasSesiones" , {headers:{"Authorization": token}})
   }
 
   getInstNombres(){
-    return Axios.get( API_URL + "instructoresNombres" /*, {headers:{"Authorization": token}}*/)
+    return Axios.get( API_URL + "instructoresNombres" , {headers:{"Authorization": token}})
   }
 
   getClasesNombres(){
     //return Axios.get( API_URL +"encontrarTodasLasClases")
     return ["Clase de boxeo", "Clase de spinning", "Clase de cardio", "Clase de yoga"]
+  }
+
+  validarInstructor(){
+    return Axios.get( API_URL +"validarInstructor", {headers:{"Authorization": token}})
   }
 }
 
