5678fff6dee2c740daebe75a6e648a00149bca62
NicolasDelgado <MY_NAME@example.comnicolas.felipedelgado@gmail.com>
2020-05-22 18:39:26 -0500

    Funcionalidades de reservas de cupos implementadas

--- fitHub-Admin/src/main/java/com/api/servicios/SesionServicio.java
+++ fitHub-Admin/src/main/java/com/api/servicios/SesionServicio.java
@@ -2,14 +2,21 @@ package com.api.servicios;
 
 import com.api.dto.SesionDTO;
 import com.api.modelos.Sesion;
+import com.api.modelos.Usuario;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public interface SesionServicio {
 
-    Sesion addSesion(SesionDTO sesionDTO);
+	Sesion addSesion(SesionDTO sesionDTO);
     List<Sesion> findAllSesionesByFecha();
     Sesion getSesionById(String idSesion);
     void deleteSesion(Sesion sesion);
+    void cambiarSesion(Sesion sesion);
+    boolean usuarioInscrito(Sesion sesion, Usuario usuario); 
+    String cancelarCupo(Sesion sesion, Usuario usuario);
 
 }
